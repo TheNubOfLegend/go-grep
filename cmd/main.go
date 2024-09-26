a43055e37e2d52eca8a2df63f1151208a3636e28
thenuboflegend <thenuboflegend@proton.me>
2024-09-26 09:07:36 -0500

    work

--- cmd/main.go
+++ cmd/main.go
@@ -14,28 +14,40 @@ func main() {
 
     var pattern []byte = []byte("at that")
     var lookup []int = make([]int, 256)
-    for i := range lookup {
-        lookup[i] = len(pattern)
-    }
+    // for i := range lookup {
+    //     lookup[i] = len(pattern)
+    // }
     for i := len(pattern) - 1; i >= 0; i-- {
-        if lookup[pattern[i]] == len(pattern) {
-            lookup[pattern[i]] = len(pattern) - i
+        if lookup[pattern[i]] == 0 {
+            lookup[pattern[i]] = len(pattern) - 1 - i
         }
     }
     for i := range lookup {
-        if lookup[i] != len(pattern) {
+        if lookup[i] != 0 {
             fmt.Printf("%c", i)
             fmt.Println(lookup[i])
         }
     }
-    offset := 72550
+    // panic("nerd")
+    // for i := 72540; i < 72580; i++ {
+    //     fmt.Printf("%c", file[i])
+    // }
+    fmt.Println()
+    offset := 0
     count := 0
-    for offset + len(pattern) < 72570 {
-        for i := len(pattern) - 1; i >= 0 && i + offset < 72600; i-- {
-            fmt.Printf("%c", file[i+offset])
-            delta := 0
+    delta := 0
+    i := len(pattern) - 1
+    for offset + len(pattern) < 72580 {
+        for i >= 0 && i + offset < 72580 {
+            if delta != 0 {
+                offset += delta + len(pattern) - 1 - i
+                i = len(pattern) - 1
+                delta = 0
+            }
+            fmt.Printf("%c ", file[i+offset])
+            fmt.Println(i + offset)
             if file[i + offset] != pattern[i] {
-                delta += lookup[file[i + offset]]
+                delta += lookup[file[i + offset]] - 1
             } else if i == 0 {
                 delta += len(pattern)
                 // for j := 0; j < len(pattern); j++ {
@@ -61,13 +73,11 @@ func main() {
                 // for l := offset + k; l < offset + j; l++ {
                 //     fmt.Printf("%c", file[offset + l])
                 // }
-            }
-            if delta != 0 {
-                offset += delta
-                i = len(pattern) - 1
-            }
+            } 
+            fmt.Printf("    d %d, i %d\n", delta, i)
             // fmt.Println(offset)
             // fmt.Printf("%c\n", file[i + offset])
+            i--
         }
         // fmt.Println(offset)
     }
